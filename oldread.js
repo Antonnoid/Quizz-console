6bfa9a9e6605b3b243c88c1f536aaa7aebb4103d
Gigorii- <chepurnov2009@yandex.ru>
2023-06-30 15:02:22 +0300

    Final Release

--- oldread.js
+++ oldread.js
@@ -1,4 +1,5 @@
 // const themeArr = read();
+const fs = require("fs");
 const Simpson = require("./simpsons");
 const Alcohole = require("./alkohole");
 const Grin = require("./grin");
@@ -9,6 +10,7 @@ const readlineSync = require("readline-sync");
 const { EOL } = require("os");
 const { readFileSync } = require("fs");
 const { arrayBuffer } = require("stream/consumers");
+const path = require("path");
 
 const userName = readlineSync.question(`Введите имя ${EOL}`);
 
@@ -19,10 +21,10 @@ const choose = readlineSync.question(`Выберите тему:
 2. Алкоголь
 3. Зеленый Слоник
 `);
+
 if (choose === "1") {
   arrResult = [...Simpson];
-}
-if (choose === "2") {
+} else if (choose === "2") {
   arrResult = [...Alcohole];
 } else {
   choose === "3";
@@ -41,22 +43,12 @@ function run() {
       console.log(`Правильный ответ: ${arrResult[i].answer}`);
       count -= 1;
     }
-    console.log(`твой результат ${count} пирожков`);
-    return count;
   }
-  run();
-  //   // let question;
-  //   // let answer;
-  //   let counter = 0;
-  //   for (let i = 0; i < themeArr.length; i++) {
-  //     const quest = readlineSync.question(`${themeArr[i][0]}${EOL}`);
-  //     if (quest === themeArr[i][1]) {
-  //       console.log('\x1b[42mУмница!', '\x1b[0m');
-  //       counter += 1;
-  //     } else {
-  //       console.log('\x1b[41mНе умница!', '\x1b[0m');
-  //     }
-  //   }
-  //   console.log(`Твои познания равны ${counter} из 5`);
+
+  console.log(`твой результат ${count} пирожков`);
+  fs.appendFileSync(
+    path.join(__dirname, "./end.txt"),
+    `${userName}: ${count} пирожка(ов)${EOL}`
+  );
 }
 run();
